001c245a3e38df0fa47b100b5fa1d1a18c00b7e5
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-15 11:10:03 -0300

    First settings

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Kim Ruan on 15/09/23.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,46 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Kim Ruan on 15/09/23.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the maket at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Kim Ruan\nAll right reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        } //: VSTACK
+        .padding()
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
