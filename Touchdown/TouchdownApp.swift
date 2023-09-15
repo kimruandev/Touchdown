23f3227368326e77e995f3f471175a391b3c0aaa
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-15 10:06:30 -0300

    Hello World!

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Kim Ruan on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
